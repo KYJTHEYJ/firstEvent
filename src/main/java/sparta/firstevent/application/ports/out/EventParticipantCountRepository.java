9588165a844827068f4e0a4e94242e142763b826
KYJ <nananan1213@gmail.com>
2026-02-26 09:27:40 +0900

    feat: eventParticipantcount 동시성 처리 중 record lock 케이스 추가

--- src/main/java/sparta/firstevent/application/ports/out/EventParticipantCountRepository.java
+++ src/main/java/sparta/firstevent/application/ports/out/EventParticipantCountRepository.java
@@ -2,6 +2,7 @@ package sparta.firstevent.application.ports.out;
 
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +13,28 @@ import java.util.Optional;
 public interface EventParticipantCountRepository extends Repository<EventParticipantCount, Long> {
     Optional<EventParticipantCount> findByEventId(Long eventId);
 
+    EventParticipantCount save(EventParticipantCount participantCount);
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT e FROM EventParticipantCount e WHERE e.eventId = :eventId")
     Optional<EventParticipantCount> findByEventIdWithLock(@Param("eventId") Long eventId);
 
-    EventParticipantCount save(EventParticipantCount participantCount);
+    @Modifying
+    @Query(
+            "UPDATE EventParticipantCount e "
+            + "SET e.participantCount = e.participantCount + 1 "
+            + ", e.updatedAt = CURRENT_TIMESTAMP "
+            + "WHERE e.eventId = :eventId"
+    )
+    int updateParticipantCount(@Param("eventId") Long eventId);
+
+    @Modifying
+    @Query(
+            "UPDATE EventParticipantCount e "
+            + "SET e.participantCount = e.participantCount + 1 "
+            + ", e.winnerCount = e.winnerCount + 1 "
+            + ", e.updatedAt = CURRENT_TIMESTAMP "
+            + "WHERE e.eventId = :eventId"
+    )
+    int updateWinnerParticipantCount(@Param("eventId") Long eventId);
 }
